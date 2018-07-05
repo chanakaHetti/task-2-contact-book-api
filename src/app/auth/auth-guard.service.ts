947f4df11ac3bbc7bfc22f4baae99233c060b45e
chanakaHetti <cdileep40@gmail.com>
2018-07-05 21:39:58 +0530

    Completed auth-gurd functionality

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,13 +1,24 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
 import { Injectable } from "@angular/core";
+
+import { Observable, of } from 'rxjs';
+
 import { AuthService } from "./auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService, private router: Router) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.authService.isAuthenticated();
-    }
+    // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    //     return this.authService.isAuthenticated();
+    // }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>|Promise<boolean>|boolean {
+        if (!this.authService.isAuthenticated()) {
+          this.router.navigate(['signin']);
+          return false;
+        }
+        return true;
+      }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  registerForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
   }
 
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+
+  private initForm() {
+    let email = '';
+    let password = '';
+
+    this.registerForm = new FormGroup({
+      'email': new FormControl(email),
+      'password': new FormControl(password)
+    }); 
+  }
 }
